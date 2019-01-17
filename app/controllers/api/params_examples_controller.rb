427b65f84317100dbed80734df9749a20c4f7136
centerpush <dylancross100@gmail.com>
2019-01-17 17:02:23 -0600

    initial commit

---
+++ app/controllers/api/params_examples_controller.rb
@@ -0,0 +1,19 @@
+class Api::ParamsExamplesController < ApplicationController
+  def string_params_example
+    @message = params[:bologna]
+    @message2 = params[:tent]
+    @bologna_message_length = @message.length
+    render 'string_example.json.jbuilder'
+  end
+
+  def url_example
+    @message = params[:wildcard]
+    render 'url_example.json.jbuilder'
+  end
+
+  def body_params_example
+    @secret_message = params[:secret_info]
+    render 'body_params_example.json.jbuilder'
+  end
+
+end
---
+++ app/views/api/params_examples/string_example.json.jbuilder
@@ -0,0 +1,3 @@
+json.message @message
+json.message2 @message2
+json.message_length @bologna_message_length
